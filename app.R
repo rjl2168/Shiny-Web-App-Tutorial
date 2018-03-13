1f2ea101cc4fdc24679ff5b258cc1a521044d72a
Rich Lavery <rjl2168@columbia.edu>
2018-03-13 10:45:35 -0400

    Updates

--- app.R
+++ app.R
@@ -8,40 +8,77 @@
 #
 
 library(shiny)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Visualizing The Central Limit Theorem"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
+         sliderInput("N",
+                     "Number of Random Variables:",
                      min = 1,
-                     max = 50,
-                     value = 30)
+                     max = 1000,
+                     value = 1),
+         
+         selectInput(inputId = "distr",
+                     label = "Distribution",
+                     choices = c("Uniform", "Exponential", "Poisson", "Chi-Squared", "Binomial", "Student-t"),
+                     selected = "Uniform")
       ),
-      
+
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("distPlot"),
+         plotOutput("qqnormPlot")
       )
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+  #Define the number of samples in each distribution
+  N_samp <- 1000
+  
    output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+     #Initialize the data frame to all zeros
+     total_dist <- as.data.frame(matrix(0,N_samp,1))
+     for (i in 1:input$N) {
+       new_dist <- switch(input$distr, 
+                          "Uniform" = runif(N_samp, min = 0, max = 10), 
+                          "Exponential" = rexp(N_samp, rate = 1),
+                          "Poisson" = rpois(N_samp, lambda = 1),
+                          "Chi-Squared" = rchisq(N_samp, df = 1),
+                          "Binomial" = rbinom(N_samp, size = 50, prob = 0.1),
+                          "Student-t" = rt(N_samp, df = 10))
+       total_dist <- total_dist + as.data.frame(new_dist)
+     }
+     #Render the plot
+     ggplot(total_dist, aes(total_dist)) + geom_histogram(aes(y = ..density..)) + stat_bin(bins = 50) + geom_density()
+     
+   })
+   
+   output$qqnormPlot <- renderPlot({
+     #Initialize the data frame to all zeros
+     total_dist <- as.data.frame(matrix(0,N_samp,1))
+     for (i in 1:input$N) {
+       new_dist <- switch(input$distr, 
+                          "Uniform" = runif(N_samp, min = 0, max = 10), 
+                          "Exponential" = rexp(N_samp, rate = 1),
+                          "Poisson" = rpois(N_samp, lambda = 1),
+                          "Chi-Squared" = rchisq(N_samp, df = 1),
+                          "Binomial" = rbinom(N_samp, size = 50, prob = 0.1),
+                          "Student-t" = rt(N_samp, df = 10))
+       total_dist <- total_dist + as.data.frame(new_dist)
+     }
+     #Render the plot
+     qqnorm(total_dist$total_dist[,N_samp])
+     qqline()
+     
    })
 }
 
