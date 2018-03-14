baf31ddddc7b67777fb4b0d16aed0f80362d1779
Rich Lavery <rich.lavery@zebra.com>
2018-03-14 13:11:20 -0400

    updates

--- app.R
+++ app.R
@@ -14,10 +14,10 @@ library(ggplot2)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Sample Distribution Example"),
+   titlePanel("Interactive Shiny Application Example"),
    
-   # Sidebar with a slider input for number of bins 
    sidebarLayout(
+      #The sidebarPanel shows the various inputs
       sidebarPanel(
          selectInput(inputId = "u",
                      label = "Mean",
@@ -26,13 +26,13 @@ ui <- fluidPage(
 
          selectInput(inputId = "std",
                     label = "Standard Deviation",
-                    choices = c(1, 5, 10, 20),
+                    choices = c(1, 3, 5),
                     selected = 1),
          
          radioButtons(inputId = "NumSamp",
                       label = "Number of Samples",
-                      choices = c(20, 100, 500, 1000),
-                      selected = 100),
+                      choices = c(100, 500, 1000),
+                      selected = 500),
 
          sliderInput(inputId = "NumBins",
                      label = "Number of Bins in Histogram",
@@ -45,7 +45,7 @@ ui <- fluidPage(
          
       ),
 
-      # Show a plot of the generated distribution
+      # The mainPanel calls out placeholders
       mainPanel(
          plotOutput("distPlot"),
          textOutput("meanValue"),
@@ -56,7 +56,7 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+   #Create a reactive object `sample_data` which can be used to other render functions
    sample_data <- reactive({
      Sample_size <- as.numeric(input$NumSamp)
      Avg <- as.numeric(input$u)
@@ -64,19 +64,20 @@ server <- function(input, output) {
      rnorm(n = Sample_size, mean = Avg, sd = Std_dev)
    })
 
+   #Create a renderPlot object to update the plot when either the `sample_data` changes or the `NumBins` changes
    output$distPlot <- renderPlot({
      Num_bins <- input$NumBins
      samples_df <- as.data.frame(sample_data())
-     ggplot(samples_df, aes(x = samples_df$sample_data)) + geom_histogram(aes(y = ..density..), stat = "bin", bins = Num_bins) + geom_density()
-     
+     ggplot(samples_df, aes(x = samples_df$sample_data)) + geom_histogram(aes(y = ..density..), stat = "bin", bins = Num_bins) + geom_density() + coord_cartesian(xlim = c(-20, 20)) + xlab("Data Values")
    })
 
+   #Create a reactive output that only updates upon observing an event, such as the user pressing a button
    observeEvent(input$showStats, {
      output$meanValue <- renderText(isolate({paste("Mean =", toString(mean(sample_data())), sep = " ")}))
      output$stdValue <- renderText(isolate({paste("Standard Deviation =", toString(sd(sample_data())), sep = " ")}))
    })
 }
 
-# Run the application 
+# Bind the user interface definition with the server definition and run the application 
 shinyApp(ui = ui, server = server)
 
